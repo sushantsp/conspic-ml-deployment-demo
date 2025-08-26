6b948c40732433a8274e554423876870bbfcacee
Sushant <32874962+sushantsp@users.noreply.github.com>
2025-08-27 00:04:32 +0530

    Update Tracking.py

--- QuickStart/Tracking.py
+++ QuickStart/Tracking.py
@@ -33,3 +33,40 @@ y_pred = lr.predict(X_test)
 
 # Calculate metrics
 accuracy = accuracy_score(y_test, y_pred)
+
+
+
+### mlflow logging
+
+# Set our tracking server uri for logging
+mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
+
+# Create a new MLflow Experiment
+mlflow.set_experiment("MLflow Quickstart")
+
+# Start an MLflow run
+with mlflow.start_run():
+    # Log the hyperparameters
+    mlflow.log_params(params)
+
+    # Log the loss metric
+    mlflow.log_metric("accuracy", accuracy)
+
+    # Infer the model signature
+    signature = infer_signature(X_train, lr.predict(X_train))
+
+    # Log the model, which inherits the parameters and metric
+    model_info = mlflow.sklearn.log_model(
+        sk_model=lr,
+        name="iris_model",
+        signature=signature,
+        input_example=X_train,
+        registered_model_name="tracking-quickstart",
+    )
+
+    # Set a tag that we can use to remind ourselves what this model was for
+    mlflow.set_logged_model_tags(
+        model_info.model_id, {"Training Info": "Basic LR model for iris data"}
+    )
+
+
